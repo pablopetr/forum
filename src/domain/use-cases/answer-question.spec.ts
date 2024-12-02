697ab2e8ce198e59d3920235550731fb1911f3a1
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-12-02 20:21:53 -0300

    Add alias and setters and getters to entities

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,7 +1,6 @@
-import {expect, test} from 'vitest'
-import { AnswerQuestionUseCase } from "./answer-question";
-import {AnswersRepository} from "../repositories/answers-repository";
-import {Answer} from "../entities/answer";
+import { AnswerQuestionUseCase } from "@/domain/use-cases/answer-question";
+import {AnswersRepository} from "@/domain/repositories/answers-repository";
+import {Answer} from "@/domain/entities/answer";
 
 const fakeAnswersRepository: AnswersRepository = {
     create: async (answer: Answer) => {
