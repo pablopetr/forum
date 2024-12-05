3734163dcf267f7d2b88bb461f3c76b309822a14
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-12-04 21:22:58 -0300

    Create edit-question use-case

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,64 @@
+import { InMemoryQuestionsRepository } from '../../../../../test/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+import { beforeEach, expect } from 'vitest'
+import { makeQuestion } from '../../../../../test/factories/make-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(1)
+
+    await sut.execute({
+      authorId: 'author-1',
+      title: 'Question Test',
+      content: 'content',
+      questionId: newQuestion.id.toValue(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Question Test',
+      content: 'content',
+      slug: Slug.create('question-test'),
+    })
+  })
+
+  it('should not be able to edit a question from another author', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(1)
+
+    await expect(
+      sut.execute({
+        authorId: 'author-2',
+        title: 'Question Test',
+        content: 'content',
+        questionId: newQuestion.id.toValue(),
+      }),
+    ).rejects.toThrowError('Not allowed')
+  })
+})
